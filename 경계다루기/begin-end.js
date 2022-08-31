a01541d581e76eb3d07d15162ecedbabc0f5113c
YH <yonghoonj98@gmail.com>
2022-08-31 10:08:41 +0900

    DELETE JS CODE

--- 경계다루기/begin-end.js
+++
@@ -1,5 +0,0 @@
-function reservationDate(beginDate, endDate) {
-  // ...some code
-}
-
-reservationDate("YYYY-MM-DD", "YYYY-MM-DD");
--- 분기다루기/값식문/값식문1.js
+++
@@ -1,8 +0,0 @@
-// This JSX:
-ReactDOM.render(<div id="msg">Hello World!</div>, mountNode);
-
-// Is transformed to this JS:
-ReactDOM.render(
-  React.createElement("div", { id: "msg" }, "Hello World!"),
-  mountNode
-);
